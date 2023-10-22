fbd4bd351c649988440678d1c144b202170bf195
Sianwa Atemi <sa.atemi@gmail.com>
2023-10-22 22:44:42 +0300

    Updated refugee participation report & its exec summary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Route, Routes, withRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Overlay from "./layouts/Overlay";
